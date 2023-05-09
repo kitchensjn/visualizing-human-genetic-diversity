0b5024c972f4d650cac93351632305020ca3d2cc
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2023-05-08 20:17:27 -0700

    initial push #1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# visualizing-human-genetic-diversity
+# Visualizing Human Genetic Diversity
+
+This repository contains the code used to create the blog post, [Visualizing Human Genetic Diversity](https://james-kitchens.com/blog/visualizing-human-genetic-diversity), on my personal website. This post revisualizes the dataset used in [Biddanda et al. 2020](https://doi.org/10.7554/eLife.60107), which can be found [here](https://datadryad.org/stash/dataset/doi:10.5061/dryad.rjdfn2z7v). Place the Dryad download within the assets folder and change its name to Dryad.
+
+generate_geovar_code.py requires the `geovar` package (see this [tutorial](https://aabiddanda.github.io/geovar/index.html)) for installation instructions. This will create a new file within assets with counts for the geovar codes.
+
+That file is an input for create_euler_diagrams.R. This code requires the `eulerr` and `UpSetR` packages. Code associated with each figure has been marked. The `eulerr` package calculates the position and shape of the ellipses within the Euler diagrams. This is not the best workflow, but these sections output a JSON object that I then copy and paste into the Markdown. Due to random starting conditions with `eulerr`, your Euler diagrams may not match those in post, but general patterns should be preserved.
+
+visualizing-human-genetic-diversity.md is the Markdown file that is used to generate the post. The section within the "---" is the front matter, which in most scenarios can be removed or ignored. Styling of the figures may be slightly changed from the output of the R code.
+
+If you have any questions, feel free to reach contact me (**James Kitchens**) at kitchensjn@gmail.com.
---
+++ generate_geovar_codes.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pkg_resources
+from geovar import *
+from tqdm import tqdm
+import gzip
+
+
+def geovar_codes_streaming_fixed(geovar_obj, freq_mat_file):
+    """Version of GeoVar code generation algorithm that streams through file to avoid memory overflow.
+    Args:
+        freq_mat_file (:obj:`string`): filepath to
+        frequency table file (see example notebook for formatting).
+    """
+    assert geovar_obj.bins is not None
+    geovar_codes = []
+    # Setting up the testing bins
+    test_bins = np.array([x[1] for x in geovar_obj.bins])
+    with gzip.open(freq_mat_file,'r') as f:
+        header = f.readline()
+        # Take the population labels currently
+        pops = np.array(header.split()[6:])
+        geovar_obj.pops = pops
+        for line in tqdm(f):
+            # Split after the 6th column ...
+            maf_vector = np.array(line.split()[6:]).astype(np.float64)
+            cur_geovar = np.digitize(maf_vector, test_bins, right=True)
+            cur_geovar_code = "".join([str(i) for i in cur_geovar])
+            geovar_codes.append(cur_geovar_code)
+    # Setting the variables here
+    geovar_obj.geovar_codes = np.array(geovar_codes)
+    geovar_obj.n_variants = geovar_obj.geovar_codes.size
+    geovar_obj.n_populations = geovar_obj.pops.size
+
+
+geovar = GeoVar(bins=[(0,0), (0,0.01), (0.01,0.05), (0.05,0.1), (0.1,1.0)])
+geovar_codes_streaming_fixed(geovar_obj=geovar, freq_mat_file="assets/Dryad/new_1kg_nyc_hg38_filt_total.biallelic_snps.pops.freq.total.txt.gz")
+counts_output = geovar.count_geovar_codes()
+geovar_code_counts = pd.DataFrame({"codes":counts_output[0],"counts":counts_output[1]})
+geovar_code_counts.to_csv("assets/Dryad/new_1kg_nyc_hg38_filt_total.biallelic_snps.pops.ncat_000_001_005_010.geodist.total.txt", sep=' ', header=False, index=False)
